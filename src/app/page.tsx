5a87e0d3bf8ee366ce62c4654cd760b73d4c73e7
Thomas Lautaro López <laautarolopez@gmail.com>
2024-03-28 23:25:14 -0300

    creacion de categorias con sus iconos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
 import lakeBg from '../../public/images/moreno-lake.jpg'
+import { MdHotel, MdInsertPhoto, MdChecklistRtl } from "react-icons/md";
+import { FaPlane, FaCar, FaFolder } from "react-icons/fa";
+import { BiWorld } from "react-icons/bi";
 
 export default function Home() {
   return (
     <div className="p-5">
       <header className="pb-5">
-        <h1 className="text-4xl font-bold mb-5">Viaje Perfecto</h1>
+        <h1 className="text-4xl font-bold mb-5">Tu próximo viaje</h1>
         <div className="bg-green-300 text-green-900 rounded-3xl py-1 px-3 w-fit">
-          En 2 dias viajas
+          En 2 dias viajás
         </div>
       </header>
       <main>
@@ -20,7 +23,32 @@ export default function Home() {
             className="absolute inset-0"
           />
           <div className="absolute inset-0 bg-green-900 opacity-70" />
-          <h2 className="relative text-3xl font-bold">Sur Argentino</h2>
+          <div className='flex flex-row items-center justify-between'>
+            <h2 className="relative text-3xl font-bold">Sur Argentino</h2>
+            <BiWorld className='relative w-9 h-9' />
+          </div>
+          <div className='flex flex-row mt-5'>
+            <div className='flex justify-center items-center bg-orange-500 w-16 h-16 mt-5 me-5 rounded-xl relative hover:cursor-pointer hover:opacity-90'>
+              <MdHotel className='w-7 h-7' />
+            </div>
+            <div className='flex justify-center items-center bg-blue-600 w-16 h-16 mt-5 me-5 rounded-xl relative hover:cursor-pointer hover:opacity-90'>
+              <FaPlane className='w-7 h-7' />
+            </div>
+            <div className='flex justify-center items-center bg-green-400 w-16 h-16 mt-5 me-5 rounded-xl relative hover:cursor-pointer hover:opacity-90'>
+              <FaCar className='w-7 h-7' />
+            </div>
+          </div>
+          <div className='flex flex-row mb-5'>
+            <div className='flex justify-center items-center bg-yellow-400 w-16 h-16 mt-5 me-5 rounded-xl relative hover:cursor-pointer hover:opacity-90'>
+              <MdInsertPhoto className='w-7 h-7' />
+            </div>
+            <div className='flex justify-center items-center bg-neutral-400 w-16 h-16 mt-5 me-5 rounded-xl relative hover:cursor-pointer hover:opacity-90'>
+              <MdChecklistRtl className='w-7 h-7' />
+            </div>
+            <div className='flex justify-center items-center bg-violet-600 w-16 h-16 mt-5 me-5 rounded-xl relative hover:cursor-pointer hover:opacity-90'>
+              <FaFolder className='w-7 h-7' />
+            </div>
+          </div>
         </div>
       </main>
       <footer></footer>
