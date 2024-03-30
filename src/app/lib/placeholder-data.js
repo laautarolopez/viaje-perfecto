5b913645a364600e46335072d9550dc4e7366973
Thomas Lautaro López <laautarolopez@gmail.com>
2024-03-30 15:30:14 -0300

    placeholder-data para usar en seed

---
+++ src/app/lib/placeholder-data.js
@@ -0,0 +1,11 @@
+const users = [
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a6442a',
+        username: 'lautaro',
+        password: '123456',
+    },
+];
+
+module.exports = {
+    users
+};
