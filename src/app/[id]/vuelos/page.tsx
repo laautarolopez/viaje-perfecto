39336c784816e547ca00fdcdfbbc21a70ba2d83e
Thomas Lautaro López <laautarolopez@gmail.com>
2024-04-01 12:42:47 -0300

    se actualizó la page de Vuelos

--- src/app/[id]/vuelos/page.tsx
+++ src/app/[id]/vuelos/page.tsx
@@ -1,16 +1,29 @@
 import CardBg from '../../components/CardBg';
 import IconsRow from '@/components/IconsRow';
+import DaysToTravel from '@/components/DaysToTravel';
+import IconButton from '@/components/IconButton'
+import { FaPlane } from 'react-icons/fa'
 
-const Vuelo = () => {
-    return (
+const Vuelo = () => (
+    <>
         <div className="p-5 mb-5 relative overflow-hidden">
             <CardBg />
             <div className="flex flex-row mt-3">
                 <h2 className="relative text-4xl font-bold">Sur Argentino</h2>
             </div>
-            <IconsRow />
+            <IconsRow activeIcon="vuelos" />
+            <DaysToTravel className="relative mt-5" />
         </div>
-    )
-}
+        <div className="p-5 pt-0">
+            <div className="flex flex-row items-center">
+                <IconButton Icon={FaPlane}
+                    iconContainerClassName="bg-blue-600 mt-0"
+                    containerWidth="w-10" containerHeight="h-10"
+                    iconWidth="w-5" iconHeight="h-5" />
+                <p className="">Vuelos</p>
+            </div>
+        </div>
+    </>
+)
 
 export default Vuelo;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import 'react-calendar/dist/Calendar.css'
 import NextTripCard from './components/NextTripCard'
 import CheckList from '@/components/CheckList'
 import CardCalendar from '@/app/components/CardCalendar'
+import DaysToTravel from '@/components/DaysToTravel'
 
 const Calendar = React.lazy(() => import('@/app/components/CardCalendar'))
 
@@ -11,9 +12,7 @@ export default function Home() {
     <div className="p-5">
       <header className="pb-5">
         <h1 className="text-4xl font-bold mb-5">Tu próximo viaje</h1>
-        <div className="bg-green-300 text-green-900 rounded-3xl py-1 px-3 w-fit">
-          En 2 dias viajás
-        </div>
+        <DaysToTravel />
       </header>
       <main>
         <NextTripCard />
