3f7abe92cd18e65862ad3918394c503fc7b6f259
Thomas Lautaro López <laautarolopez@gmail.com>
2024-04-05 16:51:31 -0300

    se cambia seed

--- src/app/lib/placeholder-data.js
+++ src/app/lib/placeholder-data.js
@@ -1,6 +1,6 @@
 const user1ID = '410544b2-4001-4271-9855-fec4b6a6442a'
 const trip2ID = '002544b2-4001-4271-9855-fec4b6a6442a'
-const trip4ID = '004544b2-4001-4271-9855-fec4b6a6442a'
+const trip4ID = 'trip-4'
 
 const users = [
   {
@@ -12,7 +12,7 @@ const users = [
 
 const trips = [
   {
-    id: '001544b2-4001-4271-9855-fec4b6a6442a',
+    id: 'trip-1',
     user_id: user1ID,
     name: 'Trip 1',
     initial_date: '2023-12-24',
@@ -20,7 +20,7 @@ const trips = [
     image: 'image1.jpg'
   },
   {
-    id: '002544b2-4001-4271-9855-fec4b6a6442a',
+    id: 'trip-2',
     user_id: user1ID,
     name: 'Trip 2',
     initial_date: '2024-12-24',
@@ -28,7 +28,7 @@ const trips = [
     image: 'image2.jpg'
   },
   {
-    id: '003544b2-4001-4271-9855-fec4b6a6442a',
+    id: 'trip-3',
     user_id: user1ID,
     name: 'Trip 3',
     initial_date: '2025-12-24',
@@ -36,11 +36,11 @@ const trips = [
     image: 'image3.jpg'
   },
   {
-    id: '004544b2-4001-4271-9855-fec4b6a6442a',
+    id: 'trip-4',
     user_id: user1ID,
-    name: 'Trip 4',
-    initial_date: '2024-06-24',
-    end_date: '2024-07-30',
+    name: 'Sur argentino',
+    initial_date: '2024-04-15',
+    end_date: '2024-04-25',
     image: 'image4.jpg'
   }
 ]
@@ -49,19 +49,28 @@ const flys = [
   {
     id: '001544b2-4001-4271-9855-fec4b6a6442a',
     fly_number: 'AR123',
-    arrival_address: 'Buenos Aires',
-    departure_date: '2023-12-24T14:00:00',
-    arrival_date: '2023-12-31T10:00:00',
-    departure_address: 'Miami',
+    arrival_address: 'Aeropuerto Ezeiza, Buenos Aires',
+    departure_date: '2024-04-15T00:00:00',
+    arrival_date: '2024-04-15T01:35:00',
+    departure_address: 'Aeropuerto Bariloche',
     trip_id: trip4ID
   },
   {
     id: '002544b2-4001-4271-9855-fec4b6a6442a',
     fly_number: 'AR124',
-    arrival_address: 'Miami',
-    departure_date: '2024-12-24T16:00:00',
-    arrival_date: '2024-12-31T11:00:00',
-    departure_address: 'Buenos Aires',
+    arrival_address: 'Aeropuerto Bariloche',
+    departure_date: '2024-04-19T16:00:00',
+    arrival_date: '2024-04-19T18:00:00',
+    departure_address: 'Aeropuerto Esquel',
+    trip_id: trip4ID
+  },
+  {
+    id: '002544b2-4001-4271-9855-fec4b6a6442a',
+    fly_number: 'AR125',
+    arrival_address: 'Aeropuerto Esquel',
+    departure_date: '2024-04-25T14:00:00',
+    arrival_date: '2024-04-24T16:15:00',
+    departure_address: 'Aeropuerto Ezeiza, Buenos Aires',
     trip_id: trip4ID
   }
 ]
