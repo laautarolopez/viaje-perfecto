9b2fcb12b4c2c0299ef53a910fbf8ed34f59cbd2
Thomas Lautaro López <laautarolopez@gmail.com>
2024-04-12 18:01:37 -0300

    blobform add

--- src/app/actions/blobActions.ts
+++ src/app/actions/blobActions.ts
@@ -8,3 +8,26 @@ export async function deleteFile(filePath: string) {
   revalidatePath(filePath)
   redirect('vuelos')
 }
+
+;('use server')
+import { del, put } from '@vercel/blob'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+export async function deleteFile(filePath: string) {
+  await del(filePath)
+  revalidatePath(filePath)
+  redirect('vuelos')
+}
+
+export async function uploadFile(file: File, trip_id: string, fly_id: string) {
+  console.log(file, 'file')
+  const filePath = `${trip_id}/vuelos/${fly_id}/${file.name}`
+  const blob = await put(filePath, file, {
+    access: 'public',
+    addRandomSuffix: false
+  })
+  revalidatePath(`${trip_id}/vuelos`)
+  redirect('vuelos')
+  return blob
+}
