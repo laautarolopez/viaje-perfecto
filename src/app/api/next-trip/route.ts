ca7bd63a0606d514d2265bf9fb70b036dbe171fd
Thomas Lautaro López <laautarolopez@gmail.com>
2024-04-16 17:25:04 -0300

    Corregido error de tipo en queryResult

--- src/app/api/next-trip/route.ts
+++ src/app/api/next-trip/route.ts
@@ -1,19 +1,19 @@
 import { query } from '@/app/lib/db'
-import { Trip } from '@/app/lib/types'
 import { NextResponse } from 'next/server'
+import { Fly, Trip } from '@/app/lib/types'
+import { QueryResult } from 'pg'
 
 //next trip endpoint
 export async function GET(request: Request) {
   const userId = request.headers.get('user_id')
 
   try {
-    const nextTripData =
-      await query(`SELECT * FROM trips WHERE initial_date > NOW() AND user_id = $1 ORDER BY initial_date ASC LIMIT 1`, [userId])
-    const nextTrip = nextTripData.rows[0]
+    const nextTripData = await query(`SELECT * FROM trips WHERE initial_date > NOW() AND user_id = $1 ORDER BY initial_date ASC LIMIT 1`, [userId]) as unknown as QueryResult<Trip>
+    const nextTrip: Trip = nextTripData.rows[0]
+    console.log(nextTrip, 'nextTripdata') 
 
-    const nextFlyData =
-      await query(`SELECT * FROM flys WHERE trip_id = $1 ORDER BY departure_date ASC LIMIT 1`, [nextTrip.id])
-    const nextFly = nextFlyData?.rows?.[0]
+    const nextFlyData = await query(`SELECT * FROM flys WHERE trip_id = $1 ORDER BY departure_date ASC LIMIT 1`, [nextTrip.id]) as unknown as QueryResult<Fly>
+    const nextFly: Fly = nextFlyData?.rows?.[0]
 
     const nextTripResponse = {
       ...nextTrip,
