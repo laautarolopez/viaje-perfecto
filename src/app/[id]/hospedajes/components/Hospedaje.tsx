7e03f7c5d4905ebb4d96885589101a5742ebd1ab
Gonzalo Cortave <gonzalocortave@amalgama.co>
2024-05-08 21:18:24 -0300

    wip

--- src/app/[id]/hospedajes/components/Hospedaje.tsx
+++ src/app/[id]/hospedajes/components/Hospedaje.tsx
@@ -1,11 +1,31 @@
-import { MdPinDrop, MdAlarmOn, MdAlarmOff, MdEventAvailable, MdEventBusy, MdAdd, MdRemove } from "react-icons/md";
-import { FaWhatsapp, FaWaze } from "react-icons/fa";
-import { FaCircleDollarToSlot } from "react-icons/fa6";
+'use client'
+
+import {
+  MdPinDrop,
+  MdAlarmOn,
+  MdAlarmOff,
+  MdEventAvailable,
+  MdEventBusy,
+  MdAdd,
+  MdRemove
+} from 'react-icons/md'
+import { FaWhatsapp, FaWaze } from 'react-icons/fa'
+import { FaCircleDollarToSlot } from 'react-icons/fa6'
 import { Hospedaje as HospedajeType } from '@/app/lib/types'
 import { format, parseISO, differenceInDays } from 'date-fns'
 import Blob from '@/components/blob/Blob'
 
-const Hospedaje = ({id, name, start_date, end_date, phone, address, price_per_night, paid, trip_id }: HospedajeType) => {
+const Hospedaje = ({
+  id,
+  name,
+  start_date,
+  end_date,
+  phone,
+  address,
+  price_per_night,
+  paid,
+  trip_id
+}: HospedajeType) => {
   const start_dateParse = parseISO(start_date)
   const start_formattedDate = format(start_dateParse, 'dd/MM/yyyy')
   const start_formattedTime = format(start_dateParse, 'HH:mm')
@@ -20,22 +40,30 @@ const Hospedaje = ({id, name, start_date, end_date, phone, address, price_per_ni
       <div className="font-bold text-2xl mt-10 text-center">{name}</div>
       <div className="flex items-center mt-16">
         <div className="flex justify-center items-center relative text-green-300 w-10 h-10 me-5">
-            <MdPinDrop className='w-10 h-10' />
+          <MdPinDrop className="w-10 h-10" />
         </div>
         <span>{address}</span>
       </div>
       <div className="grid grid-cols-2 gap-5">
-        <a className="flex items-center justify-center mt-5 border border-green-300 rounded-xl px-5 py-2" href={"https://wa.me/549" + phone} target='_blank'>
+        <a
+          className="flex items-center justify-center mt-5 border border-green-300 rounded-xl px-5 py-2"
+          href={'https://wa.me/549' + phone}
+          target="_blank"
+        >
           <div className="flex justify-center items-center relative text-green-300 w-5 h-5 me-3">
-              <FaWhatsapp className='w-5 h-5' />
+            <FaWhatsapp className="w-5 h-5" />
           </div>
-          <span className='text-green-300'>{phone}</span>
+          <span className="text-green-300">{phone}</span>
         </a>
-        <a className="flex items-center justify-center mt-5 border border-green-300 rounded-xl px-5 py-2" href={"https://google.com/maps/search/" + name + address} target='_blank'>
+        <a
+          className="flex items-center justify-center mt-5 border border-green-300 rounded-xl px-5 py-2"
+          href={'https://google.com/maps/search/' + name + address}
+          target="_blank"
+        >
           <div className="flex justify-center items-center relative text-green-300 w-5 h-5 me-3">
-              <FaWaze className='w-5 h-5' />
+            <FaWaze className="w-5 h-5" />
           </div>
-          <span className='text-green-300'>Comenzar</span>
+          <span className="text-green-300">Comenzar</span>
         </a>
       </div>
       <div className="grid grid-cols-2 gap-5 mt-10">
@@ -69,18 +97,24 @@ const Hospedaje = ({id, name, start_date, end_date, phone, address, price_per_ni
         <div className="col-span-1"></div>
         <div className="col-span-8">Precio por noche:</div>
         <div className="col-span-3">${price_per_night}</div>
-        <div className="col-span-1 text-green-300 flex items-center"><MdRemove className="w-5 h-5" /></div>
+        <div className="col-span-1 text-green-300 flex items-center">
+          <MdRemove className="w-5 h-5" />
+        </div>
         <div className="col-span-8">Precio total:</div>
         <div className="col-span-3">${total}</div>
-        <div className="col-span-1 text-green-300 flex items-center mt-3"><MdAdd className="w-5 h-5" /></div>
+        <div className="col-span-1 text-green-300 flex items-center mt-3">
+          <MdAdd className="w-5 h-5" />
+        </div>
         <div className="col-span-8 mt-3">Pagado:</div>
         <div className="col-span-3 mt-3">${paid}</div>
         <hr className="col-span-12 w-full border-green-300 mt-5" />
-        <div className="col-span-1 text-green-300 flex items-center mt-5"><FaCircleDollarToSlot className='w-5 h-5' /></div>
+        <div className="col-span-1 text-green-300 flex items-center mt-5">
+          <FaCircleDollarToSlot className="w-5 h-5" />
+        </div>
         <div className="col-span-8 text-lg ps-1 mt-5">Saldo a abonar: </div>
         <div className="col-span-3 text-lg font-bold mt-5">${total - paid}</div>
       </div>
-      {/* <Blob trip_id={trip_id} folder={`${trip_id}/hospedajes/${id}`} /> */}
+      <Blob trip_id={trip_id} folder={`${trip_id}/hospedajes/${id}`} />
     </>
   )
 }
--- src/components/blob/Blob.tsx
+++ src/components/blob/Blob.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import BlobForm from './BlobForm'
 import { ListBlobResultBlob, list } from '@vercel/blob'
 import BlobFiles from './BlobFiles'
+import { useEffect, useState } from 'react'
 
 type BlobProps = {
   trip_id: string
@@ -8,12 +11,19 @@ type BlobProps = {
 }
 
 const Blob = async ({ trip_id, folder }: BlobProps) => {
-  async function allFiles() {
-    const blobs = await list({ prefix: folder })
-    return blobs.blobs
-  }
+  // async function allFiles() {
+  //   const blobs = await list({ prefix: folder })
+  //   return blobs.blobs
+  // }
+  const [files, setFiles] = useState<ListBlobResultBlob[]>([])
+  useEffect(() => {
+    const fetchFiles = async () => {
+      const blobs = await allFiles()
+      setFiles(blobs)
+    }
 
-  const files = await allFiles()
+    fetchFiles()
+  }, [])
 
   return (
     <>
