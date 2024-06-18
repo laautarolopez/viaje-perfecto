7a45485156c05cdc9709972de46969ee1431aae7
Thomas Lautaro López <laautarolopez@gmail.com>
2024-06-18 19:28:56 -0300

    se inicializa el cron cuando se levanta el servidor

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,19 @@
+const startCron = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/start-cron', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      console.error('Failed to start cron job');
+    }
+  } catch (error) {
+    console.error('Error cron job:', error);
+  }
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -8,6 +24,12 @@ const nextConfig = {
             port: '',
           },
         ],
+    },
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        startCron()
+      }
+      return config;
     },
 };
 
