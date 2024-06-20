7aedf57fa475d5d5f0cf774d568bb3d9a8f0f310
Thomas Lautaro López <laautarolopez@gmail.com>
2024-06-20 16:56:28 -0300

    fecha de DaysToTravel corregidas

--- src/components/DaysToTravel.tsx
+++ src/components/DaysToTravel.tsx
@@ -1,16 +1,14 @@
+import { parseISO, differenceInDays } from 'date-fns'
+
 type DaysToTravelProps = {
   className?: string
   initialDate: string
 }
 
 const DaysToTravel = ({ className, initialDate }: DaysToTravelProps) => {
-  const today = new Date(
-    new Date().toLocaleString('en-US', {
-      timeZone: 'America/Argentina/Buenos_Aires'
-    })
-  ) //esto puede romper, tuve que hacerlo porque tengo problemas con mi computadora con los dias! Chequear si a vos te funciona bien.
-  const initialDateParsed = new Date(initialDate)
-  const daysToTravel = Math.floor(initialDateParsed.getDate() - today.getDate())
+  const initialDateParse = parseISO(initialDate)
+  const daysToTravel = differenceInDays(initialDateParse, new Date())
+  
   return (
     <div
       className={
