891c7a386381ce5471e7d8fd06f639c8b91fff3f
Thomas Lautaro López <laautarolopez@gmail.com>
2024-06-24 21:20:19 -0300

    al clickear la notificacion te redirecciona a '/notifications'

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,29 @@ self.addEventListener('push', function (event) {
   const { title, message } = event.data.json()
   const options = {
     body: message,
-    icon: '/images/logo.jpeg'
+    icon: '/images/logo.jpeg',
+    data: {
+      url: '/notifications'
+    }
   }
   event.waitUntil(self.registration.showNotification(title, options))
 })
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close()
+
+  event.waitUntil(
+      clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+          for (var i = 0; i < clientList.length; i++) {
+              var client = clientList[i];
+              if (client.url.includes('/notifications') && 'focus' in client) {
+                  return client.focus();
+              }
+          }
+
+          if (clients.openWindow) {
+              return clients.openWindow('/notifications');
+          }
+      })
+  );
+});
